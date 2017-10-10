03b3e17bf6bea89e1e1af8693ef9703eccb3f28e
Jason Etcovitch <jasonetco@gmail.com>
2017-10-09 21:17:59 -0400

    Add fixtures

---
+++ __tests__/fixtures/files/labels.js
@@ -0,0 +1,8 @@
+// Hi!
+
+// @todo This title has labels +example +label
+// sdfasd
+// sdfas
+// dsfsa
+
+// sdfsdaf
--- __tests__/fixtures/payloads.js
+++ __tests__/fixtures/payloads.js
@@ -26,6 +26,34 @@ exports.basic = {
   }
 }
 
+exports.labels = {
+  event: 'push',
+  payload: {
+    ref: 'refs/heads/master',
+    commits: [
+      {
+        id: 'f7d286aa6381bbb5045288496403d9427b0746e2',
+        added: [],
+        removed: [],
+        modified: ['labels.js']
+      }
+    ],
+    head_commit: {
+      id: 'f7d286aa6381bbb5045288496403d9427b0746e2'
+    },
+    pusher: {
+      name: 'JasonEtco'
+    },
+    repository: {
+      name: 'test',
+      owner: { login: 'JasonEtco' }
+    },
+    installation: {
+      id: 10000
+    }
+  }
+}
+
 exports.caseinsensitive = {
   event: 'push',
   payload: {
